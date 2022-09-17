a39683c971fb403c59f934c55a92d10da0d3b441
Vinit Raut <vinit.coder@gmail.com>
2022-09-18 00:10:14 +0530

    Migrated scripts, helpers, constants files to new folder structure

---
+++ scripts/scripts/profile.js
@@ -0,0 +1,32 @@
+import { SUPER_USER } from './constants.js';
+
+import {
+  getProfileDiffs,
+  getSelfUser,
+  getUser,
+  wantedData,
+  createCard,
+} from '../helpers/profile.js';
+
+const self_user = await getSelfUser();
+
+if (self_user?.roles[SUPER_USER]) {
+  const { profileDiffs } = await getProfileDiffs();
+  if (profileDiffs === undefined || profileDiffs.length === 0) {
+    document.getElementById('loader').innerHTML = 'No Profile Diffs !!!';
+  } else {
+    profileDiffs.forEach(async (profileDiff) => {
+      const { userId } = profileDiff;
+
+      const user = await getUser(userId);
+      const { username } = user;
+
+      const { id, ...oldData } = wantedData(user);
+      const { id: profileDiffId, ...newData } = wantedData(profileDiff);
+
+      createCard({ oldData, newData, userId, username, profileDiffId });
+    });
+  }
+} else {
+  document.getElementById('loader').innerHTML = 'You are not authorized !';
+}
