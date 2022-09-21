59b2a51b94ec3b8a52da7efaf5a97df8c179aedb
Vinit Raut <vinit.coder@gmail.com>
2022-09-22 00:33:05 +0530

    Remove: src folder

--- src/helpers/loadENV.js
+++
@@ -1,3 +0,0 @@
-window.location.hostname !== 'dashboard.realdevsquad.com'
-  ? (window.API_BASE_URL = 'https://staging-api.realdevsquad.com')
-  : (window.API_BASE_URL = 'https://api.realdevsquad.com');
